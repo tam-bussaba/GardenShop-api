67a9ff5e239dee672bc05b7486c0959d24269f91
bussaba <bussaba2875@gmail.com>
2022-06-13 17:43:41 +0700

    TestProject

---
+++ src/main/java/com/it/GardenShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenShopApiApplication.class, args);
+	}
+
+}
